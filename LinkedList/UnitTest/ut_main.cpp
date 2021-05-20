35042548bf2390569cac1232ecb69b37afe33059
Warren Hein <warren.hein@yahoo.com>
2021-05-19 17:04:51 -0700

    Added ut_main.cpp

---
+++ LinkedList/UnitTest/ut_main.cpp
@@ -0,0 +1,13 @@
+//
+//  ut_main.cpp
+//  HelloWorld
+//
+//  Created by Warren Hein on 3/5/21.
+//
+
+#include "CppUTest/CommandLineTestRunner.h"
+
+int main(int ac, char** av)
+{
+    return CommandLineTestRunner::RunAllTests(ac, av);
+}
