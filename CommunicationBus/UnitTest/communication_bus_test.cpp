e8eb2b3feb14e7e5d7b2a3c73360c5191eab8308
Warren Hein <warren.hein@yahoo.com>
2021-06-17 17:24:11 -0700

    Some cleanup of the fake for HAL_BUSx_GetBytes and initial test for IRQ handler.

--- CommunicationBus/UnitTest/communication_bus_test.cpp
+++ CommunicationBus/UnitTest/communication_bus_test.cpp
@@ -5,18 +5,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <strings.h>
 #include "CppUTest/TestHarness.h"
 #include "fff.h"
 DEFINE_FFF_GLOBALS;
 
 extern "C" {
 #include "communication_bus.h"
+}
+static uint8_t getBytesBuffer[BUFSIZE];
 
+extern "C" {
 extern busname_t BUS0;
 extern uint8_t buffer[BUFSIZE];
 extern uint8_t *bufEnd;
 extern uint8_t *bufStart;
-
 
 	FAKE_VOID_FUNC(HAL_BUSx_Statehandler, busname_t *);
 	FAKE_VALUE_FUNC(uint8_t, HAL_BUSx_GetRxCount, busname_t *);
@@ -25,9 +28,13 @@ extern uint8_t *bufStart;
     FAKE_VOID_FUNC(xTaskSignal, uint32_t);
 
 	FAKE_VALUE_FUNC(uint8_t, HAL_BUSx_GetBytes, busname_t *, uint8_t *, uint8_t);
-    uint8_t fake_HAL_BUSx_GetBytes(busname_t *bus, uint8_t *buf, uint8_t bytesRead) {
-        sprintf((char *)buf, "abc");
-        return(3);
+    uint8_t fake_HAL_BUSx_GetBytes(busname_t *bus, uint8_t *buf, uint8_t bytesToRead) {
+        uint8_t count = 0;
+        while(*bufStart!='\0' && (bytesToRead--)) {
+        	*buf++ = *bufStart++;
+        	count++;
+        }
+        return count;
     }
 }
 
@@ -41,10 +48,19 @@ TEST_GROUP(comm_bus)
         buffer[0] = '\0';
         bufStart = buffer;
         bufEnd = buffer;
+        bzero(buffer, BUFSIZE);
+        bzero(getBytesBuffer, BUFSIZE);
     }
 
     void teardown() override
     {
+    }
+
+    void addCharsToBuffer(const char *bufData)
+    {
+        while (*bufData != '\0') {
+        	*bufEnd++ = (uint8_t)*bufData++;
+        }
     }
 };
 
@@ -53,12 +69,20 @@ TEST(comm_bus, check_operation)
 	LONGS_EQUAL(3,3);
 }
 
-TEST(comm_bus, check_fake_for_HAL_BUSx_GetBytes)
+TEST(comm_bus, getBytes_returns_bytes_added_to_buffer)
 {
     uint8_t byteCount;
     uint8_t myBuf[16] = {0};
-    HAL_BUSx_GetRxCount_fake.return_val = 3;
+    addCharsToBuffer("abc");
     byteCount = HAL_BUSx_GetBytes(&BUS0, myBuf, 16);
     LONGS_EQUAL(3, byteCount);
     STRCMP_EQUAL("abc", (char *)myBuf);
+}
+
+TEST(comm_bus, irq_handler_adds_bytes_to_buffer)
+{
+    addCharsToBuffer("abcdef");
+    HAL_BUSx_GetRxCount_fake.return_val = 3;
+    BUS0_IRQHandler();
+    STRCMP_EQUAL("abc", (char *)buffer);
 }
